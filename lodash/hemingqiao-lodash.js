59c617bb84c29c149860a1234e8db215b89ec414
Heming <qiaohemingfcb@gmail.com>
2020-12-08 15:03:54 +0800

    add folder lodash

---
+++ lodash/hemingqiao-lodash.js
@@ -0,0 +1,17 @@
+/*! *****************************************************************************
+@author Heming
+founded at 2020-12-08 15:01:56
+created by WebStorm
+description: 简单实现lodash中的一些方法
+***************************************************************************** */
+
+const hemingqiao = (function (){
+  return {
+    foo,
+  };
+
+
+  function foo() {
+    console.log("foo");
+  }
+})();
