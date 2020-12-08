7440e7392f6bdcbf7ec5cb7b45a83e759df583fd
Heming <qiaohemingfcb@gmail.com>
2020-12-08 16:04:39 +0800

    update hemingqiao-lodash.js

--- lodash/hemingqiao-lodash.js
+++ lodash/hemingqiao-lodash.js
@@ -7,13 +7,17 @@ description: 简单实现lodash中的一些方法
 
 const hemingqiao = (function (){
   return {
-    chunk
+    chunk,
+    compact,
+    concat,
+    difference
   };
 
   /**
    * Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
    * @param {number[]} arr
    * @param size
+   * @return {[]}
    */
   function chunk(arr, size = 1) {
     let res = [];
@@ -38,6 +42,66 @@ const hemingqiao = (function (){
     res.push(temp);
 
     return res;
+  }
+
+
+  /**
+   * Creates an array with all falsy values removed. The values false, null, 0, "", undefined, and NaN are falsy.
+   * @param {number[]} arr
+   * @return {[]}
+   */
+  function compact(arr) {
+    let res = [];
+    for (let e of arr) {
+      if (e) res.push(e);
+    }
+    return res;
+  }
+
+
+  /**
+   * Creates a new array concatenating array with any additional arrays and/or values.
+   * @param {number[]} arr
+   * @param args
+   * @return {*}
+   */
+  function concat(arr, ...args) {
+    const res = arr.slice();
+    for (let arg of args) {
+      // 只拉平一层
+      if (Array.isArray(arg)) {
+        for (let e of arg) {
+          res.push(e);
+        }
+      } else {
+        res.push(arg);
+      }
+    }
+    return res;
+  }
+
+
+  // 返回给定数组array和传入的数组之间的差集(a - b)。
+  // 一般地，记A和B是两个集合，则所有属于A且不属于B的元素构成的集合,叫做集合A和集合B的差集。
+  /**
+   * Creates an array of array values not included in the other given arrays using SameValueZero for equality comparisons. The order and references of result values are determined by the first array.
+   * @param {number[]} array
+   * @param values
+   */
+  function difference(array, ...values) {
+    const set = new Set(array);
+    for (let value of values) {
+      if (!Array.isArray(value)) {
+        throw new TypeError("argument should be an array");
+      }
+      for (let e of value) {
+        if (array.includes(e)) {
+          set.delete(e);
+        }
+      }
+    }
+
+    return [...set];
   }
 
 })();
