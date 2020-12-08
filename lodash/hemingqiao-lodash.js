44b70b16ac2e08ef63bda17dd33d5e11477eae39
Heming <qiaohemingfcb@gmail.com>
2020-12-08 15:29:08 +0800

    update hemingqiao-lodash.js

--- lodash/hemingqiao-lodash.js
+++ lodash/hemingqiao-lodash.js
@@ -7,11 +7,40 @@ description: 简单实现lodash中的一些方法
 
 const hemingqiao = (function (){
   return {
-    foo,
+    chunk
   };
 
+  /**
+   * Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+   * @param {number[]} arr
+   * @param size
+   */
+  function chunk(arr, size = 1) {
+    let res = [];
+    if (size <= 0 || arr == undefined || arr.length === 0) return res;
+    let len = arr.length;
+    if (size > len) {
+      res.push(arr);
+      return res;
+    }
+    let j = 0;
+    let temp = [];
+    for (let i = 0; i < len; i++) {
+      if (j++ < size) {
+        temp.push(arr[i]);
+      } else {
+        res.push(temp);
+        j = 0;
+        temp = [arr[i]];
+        j++;
+      }
+    }
+    res.push(temp);
 
-  function foo() {
-    console.log("foo");
+    return res;
   }
+
 })();
+
+let res = hemingqiao.chunk([32, 64, 1024, 2048], 1)
+console.log(res);
